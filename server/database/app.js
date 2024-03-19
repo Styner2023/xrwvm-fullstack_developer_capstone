7d4c0e710f965c1c0a7cce314cec894f646a3d32
styner2023 <k.stiner22@aol.com>
2024-03-19 02:34:49 -0400

    Added Mongoose API endpoints and loaded dealership and review data

--- server/database/app.js
+++ server/database/app.js
@@ -8,11 +8,25 @@ const port = 3030;
 app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
-const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
+// const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
+// const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+const reviews_data = JSON.parse(fs.readFileSync("./data/reviews.json", 'utf8'));
+const dealerships_data = JSON.parse(fs.readFileSync("./data/dealerships.json", 'utf8'));
 
+const connectWithRetry = () => {
+    console.log('MongoDB connection with retry')
+    mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'})
+      .then(()=>{
+        console.log('MongoDB is connected')
+      })
+      .catch(err=>{
+        console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+        setTimeout(connectWithRetry, 5000)
+      })
+  }
+
+// mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
 const Reviews = require('./review');
 
