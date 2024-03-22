d280f6d4e7e27d6e406e7ecf45bb44d0ffd99962
styner2023 <k.stiner22@aol.com>
2024-03-22 16:51:12 -0400

    Built CarMake and CarModel Django models, registered them with admin site

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+from django.utils.timezone import now  
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -9,26 +9,28 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # CarMake model
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    
-    def __str__(self):
-        return self.name
-        
-# CarModel model
+  name = models.CharField(max_length=100)
+  description = models.TextField()
+
+  def __str__(self):
+    return self.name
+
 class CarModel(models.Model):
-    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=50, choices=[
-        ('sedan', 'Sedan'),
-        ('suv', 'SUV'),
-        ('wagon', 'Wagon')])
-    year = models.IntegerField(
-        validators=[
-            MinValueValidator(2015),
-            MaxValueValidator(2023)
-        ]
-    )
-    
-    def __str__(self):
-        return f"{self.make} {self.name}"
+  car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+  name = models.CharField(max_length=100)
+  CAR_TYPES = [
+      ('SEDAN', 'Sedan'),
+      ('SUV', 'SUV'),
+      ('WAGON', 'Wagon'),
+      # Add more choices as required
+  ]
+  type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+  year = models.IntegerField(default=2023,
+      validators=[
+          MaxValueValidator(2023),
+          MinValueValidator(2015)
+      ])
+  # Other fields as needed
+
+  def __str__(self):
+    return self.name  # Return the name as the string representation
