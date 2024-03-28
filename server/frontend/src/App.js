73aeb88f0a736c2feef6a746acda269136fd914f
styner2023 <k.stiner22@aol.com>
2024-03-28 03:02:05 -0400

    Added PostReview route and updated urls

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,11 @@
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 import Register from "./components/Register/Register";
 import LoginPanel from "./components/Login/Login";
 import RootPage from "./components/RootPage"; // Import your RootPage component
 import LogoutPage from "./components/LogoutPage"; // Import your LogoutPage component
 import { Routes, Route } from "react-router-dom";
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
     return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/logout" element={<LogoutPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dealers" element={<Dealers/>} />
+        <Route path="/dealer/:id" element={<Dealer/>} />
+        <Route path="/postreview/:id" element={<PostReview/>} />
       </Routes>
     );
 }
