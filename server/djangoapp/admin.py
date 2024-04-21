cd60946922a870c600c1f3196e2b8d16f55941f1
styner2023 <k.stiner22@aol.com>
2024-04-21 08:50:32 -0400

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CarMake, CarModel
+from .models import CarMake, CarModel, Dealership
 
 # Register your models here. 
 
@@ -15,6 +15,11 @@ class CarModelAdmin(admin.ModelAdmin):
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
+class DealershipModelAdmin(admin.ModelAdmin):
+    model = Dealership
+    list_display = ['name', 'address']
+
 # Register models here  
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(Dealership, DealershipModelAdmin)
